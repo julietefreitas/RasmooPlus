1acef64c72c0e90d8284318de40620acdc2bd863
JuG <juliete.gomes17@gmail.com>
2023-10-08 18:23:20 -0300

    feat: add concentraação de exception handlers

---
+++ src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -0,0 +1,23 @@
+package com.client.ws.rasmooplus.exception.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.client.ws.rasmooplus.exception.NotFoundException;
+
+@RestControllerAdvice
+public class ResourceHandler {
+	
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<String> notFoundException(NotFoundException n){
+		String error = n.getMessage();
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		
+	}
+		
+	
+
+
+}
