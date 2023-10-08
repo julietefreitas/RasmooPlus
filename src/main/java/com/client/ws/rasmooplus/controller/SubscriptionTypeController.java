f0920b06cf6b1be8c4630e8cf0b02425a7497b22
JuG <juliete.gomes17@gmail.com>
2023-10-08 17:58:02 -0300

    feat: add exceptions e adequando controllers

--- src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -5,23 +5,37 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.client.ws.rasmooplus.entity.SubscriptionType;
-import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
+import com.client.ws.rasmooplus.exception.NotFoundException;
+import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 
 @RestController
 @RequestMapping("/subscription-type")
 public class SubscriptionTypeController {
-	
+
 	@Autowired
-	private SubscriptionTypeRepository subscriptionRepository;
-	
-	
+	private SubscriptionTypeService subTypeService;;
+
+	@GetMapping("")
 	public ResponseEntity<List<SubscriptionType>> findAll() {
-		return ResponseEntity.status(HttpStatus.OK).body(subscriptionRepository.findAll());
-		
+		return ResponseEntity.status(HttpStatus.OK).body(subTypeService.findAll());
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<SubscriptionType> findById(@PathVariable("id") Long id) {
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(subTypeService.findById(id));
+
+		} catch (NotFoundException n) {
+
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
 	}
 
 }
--- src/main/java/com/client/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
+++ src/main/java/com/client/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
@@ -1,14 +1,21 @@
 package com.client.ws.rasmooplus.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.client.ws.rasmooplus.entity.SubscriptionType;
+import com.client.ws.rasmooplus.exception.NotFoundException;
+import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
 import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 
 @Service
 public class SubscriptionTypeServiceImpl implements SubscriptionTypeService{
+	
+	@Autowired
+	private SubscriptionTypeRepository subRepository;
 
 	@Override
 	public List<SubscriptionType> findAll() {
@@ -18,8 +25,12 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService{
 
 	@Override
 	public SubscriptionType findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<SubscriptionType> sub = subRepository.findById(id);
+		
+		if(sub.isEmpty()) {
+			throw new NotFoundException("Não encontrado");
+		}
+		return sub.get();
 	}
 
 	@Override
